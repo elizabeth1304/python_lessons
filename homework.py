5a425730d02a41f4f94555cd4c62bc5ccc08d7f9
Elizabeth <elizabeth04mkhitaryan@gmail.com>
2021-09-25 15:05:14 +0400

    smth

---
+++ homework.py
@@ -0,0 +1,48 @@
+# First Homework for the First lesson, sorry for the delay.
+# print("name: John", "surname: Dohn", sep=("\n"))
+
+# First Homework for the Second lesson (first part).
+# print("Twinkle, twinle, little star,", "\tHow I wonder what you are!", 
+		# "\t\tUp above the world so high,", "\t\tLike a diamond in the sky.", 
+		# "Twinkle, twinle, little star,", "\tHow I wonder what you are!", sep=("\n"))
+
+# Second Homework for the Second lessons (first part).
+# print('Hello Python world, where "\\n" is used for moving new line and 
+        # "\\t" is used for a tab.')
+
+# Third Homework for the Second lessons(first part).
+# print("\tResume (CV)", "Name: Elizabeth", "Surname: Mkhitaryan", 
+	  # "Date of birth: April 4, 2001", 
+	  # "Nationality: Armenia", "Address: Vratsakan 4th lane 5", 
+	  # "Email: elizabeth04mkhitaryan@gmail.com", "Phone: +374164030", " ", 
+	  # "\tEducation", "Yerevan", "2007-2016", "Primary School", "N 27 school", " ", 
+	  # "Yerevan", "2016-2020", "Computer Science", 
+	  # "National Polytechnic University of Armenia (college)", " ", "Yerevan", 
+	  # "2020-Present", "Information Technology", 
+	  # "National Polytechnic University of Armenia", sep=("\n"))
+
+# First Homework for the Second lesson (second part).
+# name = input("Input the Celsius degree to see what degree it'll be in Fahrenheit:\n\t")
+# print("The Fahrenheit degree: ", (int(name)*9/5)+32)
+
+# Second Homework for the Second lessons (second part).
+# a = 5
+# b = 6
+# c = 7
+# the_average = (a+b+c)/3
+# the_degrees_summary = ((a**c) + (b**c))
+# adding_squares = (a+b)**c
+# sum_multiple = int(str(a)+str(b)+str(c)) + a*b*c
+# print("The average of a, b and c is equal to ", the_average, "\n", 
+# 	"The a elevated c degree plus b elevated c degree is equal to ", the_degrees_summary, "\n",
+# 	"The a plus b elevated c degree equal to ", adding_squares, "\n",
+# 	"The abc plus a times b times c is equal to ", sum_multiple)
+
+# Third Homework for the Second lessons(second part).
+# name = input("Input your heaight to see your optimal weight:\n\t")
+# print("The optimal weight will be: ", (int(name)-110)*1.15)
+
+# The EXTRA task will come later or maybe never.
+
+
+
